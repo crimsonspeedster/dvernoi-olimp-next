0cf72a34564658c9fc6d85170d89ccc6de175e54
Oleg <3ejiehuu2@gmail.com>
2022-11-04 14:01:31 +0200

    [+] added ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: 'ui',
+            exec_mode: 'cluster',
+            instances: 1, // Or a number of instances
+            script: 'npm',
+            args: 'start',
+            env: {
+                PORT: '3010',
+            }
+        }
+    ]
+}
