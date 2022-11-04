ec72029aed62183a42dceb2d72c1e79f20a1c0ca
Oleg <3ejiehuu2@gmail.com>
2022-11-04 14:47:01 +0200

    [*] changed port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
             script: 'npm',
             args: 'start',
             env: {
-                PORT: '3000',
+                PORT: '3010',
             }
         }
     ]
