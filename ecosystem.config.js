93b91ca1710574d13b4bcbe3e05e1829d3e79676
Oleg <3ejiehuu2@gmail.com>
2022-11-04 14:12:15 +0200

    [*] changed ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             exec_mode: 'cluster',
             instances: 1, // Or a number of instances
             script: 'npm',
-            args: 'start',
+            args: 'dev',
             env: {
                 PORT: '3010',
             }
