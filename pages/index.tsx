42ae569a506ce548003e3b4c1795cdea45b416f7
Oleg <3ejiehuu2@gmail.com>
2022-11-05 15:12:12 +0200

    [*] returned back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {GetServerSideProps, GetStaticProps} from "next";
+import {GetServerSideProps} from "next";
 import {getApolloClient} from "@services/graphql/conf/apolloClient";
 import axios from "axios";
 import {GetMenu} from "@components/Layout/graphql";
@@ -11,7 +11,7 @@ import HomeTemplate from "@root/templates/Home";
 interface HomeProps {
     pageData: any,
     settingsData: any,
-    // menus: any,
+    menus: any,
     recentPosts: any
 }
 
@@ -20,45 +20,42 @@ const Home:React.FC<HomeProps> = (props) => {
     const {
         pageData,
         settingsData,
-        // menus,
+        menus,
         recentPosts
     } = props;
 
-    console.log(pageData);
-
     return (
         <>
-            {/*<SettingsContext.Provider value={{*/}
-            {/*    settings: settingsData,*/}
-            {/*    translates: pageData.translated_slugs,*/}
-            {/*    menus*/}
-            {/*}}>*/}
-            {/*    <Layout>*/}
-            {/*        <HeadHTML seoPage={pageData.yoast_head_json} />*/}
+            <SettingsContext.Provider value={{
+                settings: settingsData,
+                translates: pageData.translated_slugs,
+                menus
+            }}>
+                <Layout>
+                    <HeadHTML seoPage={pageData.yoast_head_json} />
 
-            {/*        <HomeTemplate*/}
-            {/*            seoBlockContent={pageData.acf.seo_block}*/}
-            {/*            work_parts={pageData.acf.work_parts}*/}
-            {/*            partners={pageData.acf.partners}*/}
-            {/*            help={pageData.acf.help}*/}
-            {/*            causes={pageData.acf.causes}*/}
-            {/*            door_group={pageData.acf.door_group}*/}
-            {/*            novelties={pageData.acf.novelties}*/}
-            {/*            last_video={pageData.acf.last_video}*/}
-            {/*            recentPosts={recentPosts}*/}
-            {/*            hit_prodazh={pageData.acf.hit_prodazh}*/}
-            {/*            top_group={pageData.acf.top_group}*/}
-            {/*        />*/}
-            {/*    </Layout>*/}
-            {/*</SettingsContext.Provider>*/}
-            <div>Home Page</div>
+                    <HomeTemplate
+                        seoBlockContent={pageData.acf.seo_block}
+                        work_parts={pageData.acf.work_parts}
+                        partners={pageData.acf.partners}
+                        help={pageData.acf.help}
+                        causes={pageData.acf.causes}
+                        door_group={pageData.acf.door_group}
+                        novelties={pageData.acf.novelties}
+                        last_video={pageData.acf.last_video}
+                        recentPosts={recentPosts}
+                        hit_prodazh={pageData.acf.hit_prodazh}
+                        top_group={pageData.acf.top_group}
+                    />
+                </Layout>
+            </SettingsContext.Provider>
         </>
     );
 }
 
 export default Home;
 
-export const getStaticProps:GetStaticProps = async ({locale}) => {
+export const getServerSideProps:GetServerSideProps = async ({locale}) => {
     const apolloClient = getApolloClient();
 
     const pageRequest = axios.get(`${process.env.NEXT_PUBLIC_ENV_APP_API}/pages/`, {
@@ -95,69 +92,69 @@ export const getStaticProps:GetStaticProps = async ({locale}) => {
         };
     }));
 
-    // const {data: footer_company} = await apolloClient.query({
-    //     query: GetMenu,
-    //     variables: {
-    //         location: 'FOOTER_COMPANY',
-    //         lang: locale
-    //     },
-    // });
-    //
-    // const {data: footer_questions} = await apolloClient.query({
-    //     query: GetMenu,
-    //     variables: {
-    //         location: 'FOOTER_QUESTIONS',
-    //         lang: locale
-    //     },
-    // });
-    //
-    // const {data: footer_catalog} = await apolloClient.query({
-    //     query: GetMenu,
-    //     variables: {
-    //         location: 'FOOTER_CATALOG',
-    //         lang: locale
-    //     },
-    // });
-    //
-    // const {data: header_top} = await apolloClient.query({
-    //     query: GetMenu,
-    //     variables: {
-    //         location: 'HEADER_TOP',
-    //         lang: locale
-    //     },
-    // });
-    //
-    // const {data: header_catalog} = await apolloClient.query({
-    //     query: GetMenu,
-    //     variables: {
-    //         location: 'HEADER_CATALOG',
-    //         lang: locale
-    //     },
-    // });
-    //
-    // const {data: catalog_menu} = await apolloClient.query({
-    //     query: GetMenu,
-    //     variables: {
-    //         location: 'CATALOG_MENU',
-    //         lang: locale
-    //     },
-    // });
+    const {data: footer_company} = await apolloClient.query({
+        query: GetMenu,
+        variables: {
+            location: 'FOOTER_COMPANY',
+            lang: locale
+        },
+    });
 
-    // const menus = {
-    //     footer_company: footer_company?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
-    //     footer_questions: footer_questions?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
-    //     footer_catalog: footer_catalog?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
-    //     header_top: header_top?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
-    //     header_catalog: header_catalog?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
-    //     catalog_menu: catalog_menu?.menus?.nodes?.[0]?.menuItems?.nodes ?? []
-    // };
+    const {data: footer_questions} = await apolloClient.query({
+        query: GetMenu,
+        variables: {
+            location: 'FOOTER_QUESTIONS',
+            lang: locale
+        },
+    });
+
+    const {data: footer_catalog} = await apolloClient.query({
+        query: GetMenu,
+        variables: {
+            location: 'FOOTER_CATALOG',
+            lang: locale
+        },
+    });
+
+    const {data: header_top} = await apolloClient.query({
+        query: GetMenu,
+        variables: {
+            location: 'HEADER_TOP',
+            lang: locale
+        },
+    });
+
+    const {data: header_catalog} = await apolloClient.query({
+        query: GetMenu,
+        variables: {
+            location: 'HEADER_CATALOG',
+            lang: locale
+        },
+    });
+
+    const {data: catalog_menu} = await apolloClient.query({
+        query: GetMenu,
+        variables: {
+            location: 'CATALOG_MENU',
+            lang: locale
+        },
+    });
+
+    const menus = {
+        footer_company: footer_company?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
+        footer_questions: footer_questions?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
+        footer_catalog: footer_catalog?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
+        header_top: header_top?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
+        header_catalog: header_catalog?.menus?.nodes?.[0]?.menuItems?.nodes ?? [],
+        catalog_menu: catalog_menu?.menus?.nodes?.[0]?.menuItems?.nodes ?? []
+    };
 
     return {
         props: {
             pageData: res.page,
             settingsData: res.settings,
             recentPosts: res.recentPosts,
-            // menus
+            menus
         }
     }
 }
