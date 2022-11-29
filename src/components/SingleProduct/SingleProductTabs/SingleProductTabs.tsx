22e70bd7a1d6ef4ee72b7369cecb70a567953b4b
Oleg <3ejiehuu2@gmail.com>
2022-11-29 10:36:23 +0200

    [*] fix for description

--- src/components/SingleProduct/SingleProductTabs/SingleProductTabs.tsx
+++ src/components/SingleProduct/SingleProductTabs/SingleProductTabs.tsx
@@ -19,92 +19,102 @@ const SingleProductTabs: React.FC<SingleProductTabsProps> = (props) => {
         description,
     } = props;
 
-    return (
-        <section className={styles['single-product-tabs']}>
-            <div className="container">
-                <Tabs className={styles['single-product-tabs__tab']} selectedTabClassName={styles['active']}>
-                    <TabList className={styles['single-product-tabs__nav']}>
-                        <Tab className={styles['single-product-tabs__nav-item']}>Описание</Tab>
+    if (description || acf.enable_characteristics || acf.video_group.enable_video)
+    {
+        return (
+            <section className={styles['single-product-tabs']}>
+                <div className="container">
+                    <Tabs className={styles['single-product-tabs__tab']} selectedTabClassName={styles['active']}>
+                        <TabList className={styles['single-product-tabs__nav']}>
+                            {
+                                description &&
+                                <Tab className={styles['single-product-tabs__nav-item']}>Описание</Tab>
+                            }
 
-                        {
-                            acf.video_group.enable_video &&
-                            <Tab className={styles['single-product-tabs__nav-item']}>Видео</Tab>
-                        }
+                            {
+                                acf.video_group.enable_video &&
+                                <Tab className={styles['single-product-tabs__nav-item']}>Видео</Tab>
+                            }
 
-                        {
-                            acf.enable_characteristics &&
-                            <Tab className={styles['single-product-tabs__nav-item']}>Характеристики</Tab>
-                        }
-                    </TabList>
+                            {
+                                acf.enable_characteristics &&
+                                <Tab className={styles['single-product-tabs__nav-item']}>Характеристики</Tab>
+                            }
+                        </TabList>
 
-                    <div className={styles['single-product-tabs__body']}>
-                        <TabPanel className={styles['single-product-tabs__body-item']}>
-                            <div className={styles['single-product-tabs__desc']}>
-                                <If condition={acf.akciya.enable}>
-                                    <Then>
-                                        <div className={styles['single-product-tabs__sale']}>
-                                            <div className={styles['single-product-tabs__sale-bg']}>
-                                                {
-                                                    new Array(3).fill('').map((_, i) => (
-                                                        <svg key={i}>
-                                                            <use href={`${sprite.src}#percent`}/>
-                                                        </svg>
-                                                    ))
-                                                }
-                                            </div>
+                        <div className={styles['single-product-tabs__body']}>
+                            {
+                                description &&
+                                <TabPanel className={styles['single-product-tabs__body-item']}>
+                                    <div className={styles['single-product-tabs__desc']}>
+                                        <If condition={acf.akciya.enable}>
+                                            <Then>
+                                                <div className={styles['single-product-tabs__sale']}>
+                                                    <div className={styles['single-product-tabs__sale-bg']}>
+                                                        {
+                                                            new Array(3).fill('').map((_, i) => (
+                                                                <svg key={i}>
+                                                                    <use href={`${sprite.src}#percent`}/>
+                                                                </svg>
+                                                            ))
+                                                        }
+                                                    </div>
 
-                                            <span className={styles['single-product-tabs__sale-label']}>Акция</span>
+                                                    <span className={styles['single-product-tabs__sale-label']}>Акция</span>
 
-                                            <h2 className={styles['single-product-tabs__sale-title']}>Акция</h2>
+                                                    <h2 className={styles['single-product-tabs__sale-title']}>Акция</h2>
 
-                                            <div className={styles['single-product-tabs__sale-desc']} dangerouslySetInnerHTML={{__html: acf.akciya.description}} />
-                                        </div>
-                                    </Then>
-                                </If>
-
-                                <div className={styles['single-product-tabs__desc-inner']} dangerouslySetInnerHTML={{__html: description}} />
-                            </div>
-                        </TabPanel>
-
-                        {
-                            acf.video_group.enable_video &&
-                            <TabPanel className={styles['single-product-tabs__body-item']}>
-                                <div className={styles['single-product-tabs__video']}>
-                                    <VideoCard
-                                        ssylka_na_video={acf.video_group.video}
-                                        tekst_pod_video={acf.video_group.video_title}
-                                        foto={acf.video_group.foto}
-                                        dlitelnost_video={acf.video_group.duration}
-                                    />
-                                </div>
-                            </TabPanel>
-                        }
-
-                        {
-                            acf.enable_characteristics &&
-                            <TabPanel className={styles['single-product-tabs__body-item']}>
-                                <div className={styles['single-product-tabs__chars']}>
-                                    <div className={styles['single-product-tabs__chars-inner']}>
-                                        {
-                                            acf.haratekristiki.map((item, i) => (
-                                                <div key={i} className={styles['single-product-tabs__chars-item-inner']}>
-                                                    <p className={styles['single-product-tabs__chars-title']}>{item.nazvanie}</p>
-
-                                                    <p className={styles['single-product-tabs__chars-value']}>{item.opisanie}</p>
+                                                    <div className={styles['single-product-tabs__sale-desc']} dangerouslySetInnerHTML={{__html: acf.akciya.description}} />
                                                 </div>
-                                            ))
-                                        }
-                                    </div>
-                                </div>
-                            </TabPanel>
-                        }
-                    </div>
-                </Tabs>
-            </div>
-        </section>
-    )
+                                            </Then>
+                                        </If>
 
-    // return (<div>test</div>);
+                                        <div className={styles['single-product-tabs__desc-inner']} dangerouslySetInnerHTML={{__html: description}} />
+                                    </div>
+                                </TabPanel>
+                            }
+
+                            {
+                                acf.video_group.enable_video &&
+                                <TabPanel className={styles['single-product-tabs__body-item']}>
+                                    <div className={styles['single-product-tabs__video']}>
+                                        <VideoCard
+                                            ssylka_na_video={acf.video_group.video}
+                                            tekst_pod_video={acf.video_group.video_title}
+                                            foto={acf.video_group.foto}
+                                            dlitelnost_video={acf.video_group.duration}
+                                        />
+                                    </div>
+                                </TabPanel>
+                            }
+
+                            {
+                                acf.enable_characteristics &&
+                                <TabPanel className={styles['single-product-tabs__body-item']}>
+                                    <div className={styles['single-product-tabs__chars']}>
+                                        <div className={styles['single-product-tabs__chars-inner']}>
+                                            {
+                                                acf.haratekristiki.map((item, i) => (
+                                                    <div key={i} className={styles['single-product-tabs__chars-item-inner']}>
+                                                        <p className={styles['single-product-tabs__chars-title']}>{item.nazvanie}</p>
+
+                                                        <p className={styles['single-product-tabs__chars-value']}>{item.opisanie}</p>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    </div>
+                                </TabPanel>
+                            }
+                        </div>
+                    </Tabs>
+                </div>
+            </section>
+        )
+    }
+    else {
+        return (<></>);
+    }
 }
 
 export default SingleProductTabs
